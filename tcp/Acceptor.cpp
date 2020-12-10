38be004e169e566b7eb74acbe68986be92624d4f
Ce Sun <sunce@ufl.edu>
2020-12-10 17:04:42 -0500

    enable SO_REUSEADDR on the server socket

--- tcp/Acceptor.cpp
+++ tcp/Acceptor.cpp
@@ -15,6 +15,9 @@ Acceptor::Acceptor(int port) {
             .sin_port = ::htons(static_cast<uint16_t>(port)),
     };
     addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+    const int REUSEADDR_TRUE = 1;
+    ::setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR,
+                 &REUSEADDR_TRUE, sizeof(REUSEADDR_TRUE));
     if (::bind(server_socket_fd, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) == -1)
         panic("bind");
     if (::listen(server_socket_fd, ACCEPT_QUEUE_SIZE) == -1)
